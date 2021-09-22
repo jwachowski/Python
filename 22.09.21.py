47b07b68d32c23a5e4473c00a8a2cf0cb1a952e1
jwachowski <91121163+jwachowski@users.noreply.github.com>
2021-09-22 11:18:48 +0200

    Add files via upload

---
+++ 22.09.21.py
@@ -0,0 +1,10 @@
+print('Wyswietl sie')
+#a = input('podaj numer karty:')  # hasztag komentarz w jednej linii
+#print(a)
+"""a = int(input('podaj a'))  trzy razy " komentowanie całego bloku
+
+b = int(input('podaj b'))
+print(a+b)"""
+imie1 = 'Tomek'
+imie2 = 'Jacek'
+print(imie1 + ' oraz ' + imie2 + ' wyrzucili Monike przez okno ')
