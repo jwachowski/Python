aee5af8cb1f405a02196768a329cef5bed1b0c29
jwachowski <91121163+jwachowski@users.noreply.github.com>
2021-09-22 12:13:40 +0200

    Add files via upload

---
+++ paczkawieksalatka.py
@@ -0,0 +1,21 @@
+#zadanie1
+"""o1 = int(input("Podaj liczbę uwzględnioną w przepisie"))
+s1 = int(input("Podaj ilość składników dla jednej osoby"))
+o2 = int(input("Ile osób przyjdzie"))
+
+print((s1 * o2) / o1  )"""
+
+#zadanie2
+"""X = int(input('podaj wiek najmlodszej osoby: '))
+Y = int(input('podaj wiek najstarszej osoby: '))
+S = float(input('Podaj srednia wieku: '))
+Z = (3 * S) - X  - Y
+print("Wiek trzeciej osoby to : ")
+print(Z)"""
+#zadanie3
+a = int(input('podaj dlugosc podlogi'))
+b = int(input('podaj szerokosc podlogi'))
+powierzchnia = a * b 
+paczka = int(input('ile metrow pokryje paczka?: '))
+wynik = powierzchnia / paczka
+print('potrzeba ' + str(wynik) + 'paczek')
